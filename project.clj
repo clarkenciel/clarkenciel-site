c0acdcbe059e537e4dc30a860e34dc34747678f4
clarkenciel <danny@clarkenciel.com>
2016-04-06 11:52:16 -0700

    Fixing cli stuff

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clarkenciel-site "0.1.0-SNAPSHOT"
+(defproject clarkenciel-site "0.1.0"
   :description "Personal site of Danny Clarke"
   :url "http://clarkenciel.com"
   :license {:name "MIT"
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.5.0"]
                  [ring/ring-json "0.4.0"]
-                 [aleph "0.4.0"]]
+                 [aleph "0.4.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main clarkenciel-site.core
   :aot [clarkenciel-site.core]
   :test-path "test/clarkenciel-site")
--- src/clarkenciel_site/core.clj
+++ src/clarkenciel_site/core.clj
@@ -1,12 +1,30 @@
 (ns clarkenciel-site.core
   (:gen-class)
   (:require [clarkenciel-site.middleware :refer [wrapped-app]]
-            [aleph.http :refer [start-server]]))
+            [aleph.http :refer [start-server]]
+            [clojure.tools.cli :refer [parse-opts]]))
 
 (defn app [port]
   (start-server #'wrapped-app {:port port}))
 
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 42
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be number between 0 and 65536"]]
+   ["-h" "--help"]])
+
 (defn -main [& args]
-  (println "running on port:" 10001)
-  (app 10001)
-  )
+  (let [{{:keys [port help]} :options errors :errors} (parse-opts args cli-options)]
+    (cond
+      help (println "java -jar clarkenciel-site -p PORTNUMBER")
+
+      (not (nil? errors))
+      (doseq [e errors] (println e))
+
+      (nil? port)
+      (println "Please provide a port number")
+      
+      :else
+      (do (println "running on port:" port)
+          (app port)))))
