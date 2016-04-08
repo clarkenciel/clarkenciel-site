fbb0ce66e14e9c549adf35a36a63e7b2d2c2eecd
clarkenciel <danny@clarkenciel.com>
2016-04-08 12:27:04 -0700

    Starting on routing

--- src/clarkenciel_site/db/core.clj
+++ src/clarkenciel_site/db/core.clj
@@ -1,6 +1,6 @@
 (ns clarkenciel-site.db.core
-  (:require [mount.core :as mount]
-            [conman.core :as conman]
+  (:require [conman.core :as conman]
+            [mount.core :refer [defstate]]
             [environ.core :refer [env]]))
 
 (def pool-spec
@@ -9,4 +9,10 @@
    :min-idle 1
    :max-idle 4
    :max-active 32
-   :jdbc-url ()})
+   :jdbc-url (:database-url env)})
+
+(defstate ^:dynamic *db*
+  :start (conman/connect! pool-spec)
+  :stop (conman/disconnect! *db*))
+
+(conman/bind-connection *db* "sql/queries.sql")
--- src/clarkenciel_site/routes.clj
+++ src/clarkenciel_site/routes.clj
@@ -1,7 +1,45 @@
 (ns clarkenciel-site.routes
-  (:require [compojure.core :refer [GET POST PUT DELETE routes]]
+  (:require [compojure.core :refer [GET POST PUT DELETE routes context]]
             [clarkenciel-site.handlers :as h]))
+
+;; TODO: revisit these routes to make them easier to block with authorization
+(def post-routes
+  (routes
+   (context "/api/posts" []
+            (GET "/" []
+                 h/all-posts-handler)            
+            (GET "/for_tag/:tag-name" [tag-name]
+                 (h/tag-posts-handler tag-name))
+            (GET "/for_user/:user-name" [user-name]
+                 (h/user-posts-handler user-name))
+            (POST "/new_post" req
+                  (h/add-post-handler req))
+            (PUT "/update_post" req
+                 (h/update-post-handler post-id req))
+            (GET "/:post-id" [post-id]
+                 (h/get-post-handler post-id)))))
+
+(def user-routes
+  (routes
+   (context "/api/users" []
+            (GET "/" []
+                 h/all-users-handler)
+            (GET "/:user-email-or-id" [user-email-or-id]
+                 (h/get-user-handler user-email-or-id))
+            (POST "/new_user" req
+                  (h/add-user-handler req))
+            (PUT "/update_user" req
+                 (h/add-user-handler req)))))
+
+(def auth-routes
+  (routes
+   (context "/api/auth" []
+            (POST "/login" req
+                  (h/login-handler req))
+            (POST "/logout" req
+                  (h/logout-handler req)))))
 
 (def app-routes
   (routes
+   
    (GET "*" [] h/home-page-handler)))
